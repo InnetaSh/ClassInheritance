30c268075b342be5231b73841b9b16001a035673
InnetaSh <inneta3@gmail.com>
2024-06-06 20:55:44 +0300

    Add class Employee, Manager

---
+++ Manager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    public class Manager : Employee
+    {
+        public string Department;
+
+        public Manager(string name, string position,string departament):base(name,position)
+        {
+            this.Department = departament;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Имя - {Name},\nдолжность - {Position}\nдепартамент - {Department}");
+        }
+    }
+   
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using ClassInheritance;
+
+Employee empl1 = new Employee("Петров","менеджер");
+empl1.Info();
+Manager manager1 = new Manager("Иванов","руководитель","отдел лингвистики");
+manager1.Info();
+
+
+
+
