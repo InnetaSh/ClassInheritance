a7edf324737f37449548d65eed3bec60a6587dc5
InnetaSh <inneta3@gmail.com>
2024-06-07 13:29:55 +0300

    Add class Animal,Dog,Cat

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+//Полиморфизм с помощью метода "Звук":
+//Напишите базовый класс Animal с методом MakeSound.
+//Создайте производные классы Dog и Cat, которые переопределяют метод MakeSound.
+//Напишите метод, который принимает объект типа Animal и вызывает его метод MakeSound.
+
+namespace ClassInheritance
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,19 @@
 
 Employee empl1 = new Employee("Петров","менеджер");
 empl1.Info();
+Console.WriteLine();
+
 Manager manager1 = new Manager("Иванов","руководитель","отдел лингвистики");
 manager1.Info();
+Console.WriteLine();
 
 
-
+void MakeSound(Animal animal)
+{
+    animal.MakeSound();
+}
+Dog dog = new Dog();
+Cat cat = new Cat();
+MakeSound(dog);
+MakeSound(cat);
 
