51e14eaa1cca63340c3b8e00a5f4d3be2089911e
InnetaSh <inneta3@gmail.com>
2024-06-07 14:38:09 +0300

    Add class Counter

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ Employee empl1 = new Employee("Петров", "менеджер");
 empl1.Info();
 Console.WriteLine();
 
+
 Manager manager1 = new Manager("Иванов", "руководитель", "отдел лингвистики");
 manager1.Info();
 Console.WriteLine();
@@ -17,6 +18,9 @@ Dog dog = new Dog();
 Cat cat = new Cat();
 MakeSound(dog);
 MakeSound(cat);
+Console.WriteLine();
+
+
 
 BankAccount account = new BankAccount(1500, "Иван");
 account.Info();
@@ -28,4 +32,23 @@ Console.WriteLine();
 
 account.TryGetMoney();
 account.Info();
+Console.WriteLine();
+
+
+
+Counter count1 = new Counter(15);
+count1.Info();
+
+Counter count2 = new Counter(20);
+count2.Info();
+count2.Increment();
+count2.Increment();
+count2.Info();
+
+Counter count3 = new Counter(25);
+count3.Info();
+count3.Decrement();
+count3.Decrement();
+count3.Info();
+
 
